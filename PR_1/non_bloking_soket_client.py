c357f725b49ce2ff2df2bf5bc7ce834d967bc5d0
tanaostaf <43130037+tanaostaf@users.noreply.github.com>
2019-10-20 20:39:43 +0300

    Add files via upload

--- PR_1/non_bloking_soket_client.py
+++ PR_1/non_bloking_soket_client.py
@@ -1,23 +1,27 @@
+
 import socket
 import threading
 import sys
-import pickle 
+import pickle
 
 class Cliente():
-	"""docstring for Cliente"""
-	def __init__(self, host = "localhost", port = 7003):
+	def __init__(self, host="localhost", port=7003):
+
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.connect((str(host),int(port)))
 		msg_recv = threading.Thread(target = self.msg_recv)
-		msg_recv.daemon = True
+		msg_recv.daemon = True 
 		msg_recv.start()
+
+
 		while True:
-			msg = input('->')
+			msg = input('-> ')
 			if msg != 'salir':
 				self.send_msg(msg)
 			else:
 				self.sock.close()
 				sys.exit()
+
 	def msg_recv(self):
 		while True:
 			try:
@@ -27,8 +31,11 @@ class Cliente():
 					print(data)
 			except:
 				pass
-	def send_msg(self,msg):
+	def send_msg(self, msg):
 		try:
 			self.sock.send(pickle.dumps(msg))
 		except:
-			print('error')	
+			print('error')
+
+
+Cliente()
--- PR_1/non_bloking_soket_server.py
+++ PR_1/non_bloking_soket_server.py
@@ -1,10 +1,11 @@
 import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+import threading
+import sys
+import pickle
 
 class Servidor():
-	"""docstring for Servidor"""
-	def __init__(self, host = "localhost", port = 7003):
+	def __init__(self, host="localhost", port=7003):
+
 		self.clientes = []
 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,32 +13,35 @@ class Servidor():
 		self.sock.listen(10)
 		self.sock.setblocking(False)
 
-		aceptar = threading.Thread(target = self.aceptarCon)
-		procesar = threading.Thread(target = self.procesarCon)
-
+		aceptar = threading.Thread(target=self.aceptarCon)
+		procesar = threading.Thread(target=self.procesarCon)
+	
 		aceptar.daemon = True
 		aceptar.start()
 
 		procesar.daemon = True
 		procesar.start()
 
-		try:
+		try: 
 			while True:
-				msg = input('->')
-				if msg == 'salir': 
+				msg = input('-> ')
+				if msg == 'salir':
 					break
 			self.sock.close()
 			sys.exit()
+			
 		except:
 			self.sock.close()
-			sys.exit()		    
+			sys.exit()
+
 	def msg_to_all(self, msg, cliente):
 		for c in self.clientes:
 			try:
-				if c != cliente:
+				if c!=cliente:
 					c.send(msg)
 			except:
 				self.clientes.remove(c)
+
 	def aceptarCon(self):
 		print("aceptarCon iniciado")
 		while True:
@@ -47,17 +51,18 @@ class Servidor():
 				self.clientes.append(conn)
 			except:
 				pass
+
 	def procesarCon(self):
 		print("procesarCon iniciado")
-		while  True:
-		 if len(self.clientes)>0:
-		 	for c in self.clientes:
-		 		try:
-		 			data = c.recv(1024)
-		 			if data:
-		 				self.msg_to_all(data,c)
-		 		except:
-		 			pass
+		while True:
+			if len(self.clientes)>0:
+				for c in self.clientes:
+					try:
+						data = c.recv(1024)
+						if data:
+							self.msg_to_all(data,c)
+					except:
+						pass	
 
 
-		
+serv = Servidor()
